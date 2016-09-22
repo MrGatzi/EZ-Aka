c80a0d03d08141e3c654a2f96adba6cf0817744d
MrGatzi <martingatzweiler@gmail.com>
2016-09-22 13:19:59 +0200

    Addded gatzis Con5

---
+++ Gatzi_Test_Side/concrete5.7.5.9/application/files/cache/0fea6a13c52b4d47/25368f24b045ca84/38a865804f8fdcb6/57cd99682e939275/2ddb27c5cdf0b672/745d4c64665be841/19d46f2548d0c4dc/cf6cbba19a6e0c03.php
@@ -0,0 +1,16 @@
+<?php 
+/* Cachekey: cache/stash_default/doctrine/doctrinenamespacecachekey[dc2_3fb2257847f85f6340082c8ea871ce69_]/ */
+/* Type: array */
+
+
+
+$loaded = true;
+$expiration = 1474851112;
+
+$data = array();
+
+/* Child Type: integer */
+$data['return'] = 1;
+
+/* Child Type: integer */
+$data['createdOn'] = 1474455293;
