c956696900ba0dd21ae40f4c04553f3a5d13c7aa
obi264 <obi264@gmail.com>
2017-01-03 20:25:43 +0100

    a boa sochn hoid

--- david/seite1/web/js/swagmode.js
+++ david/seite1/web/js/swagmode.js
@@ -8,63 +8,88 @@ Description: This is the Swagmode code
 //either as a normal multi-level page or a one-page.
 function swagmodeon(){
   $('.homebutton').click(function(){
-    $('.about').hide("slow");
-    $('.contact').hide("slow");
-    $('.home').show("slow");
-    $('.services').hide("slow");
-    $('.howto').hide("slow");
-    $('.goodpractice').hide("slow");
-	   window.scrollTo(0, 0);
+    $('body').animate({
+        scrollTop: 0
+    }, 600, function(){
+      $('.about').hide("slow");
+      $('.contact').hide("slow");
+      $('.home').show("slow");
+      $('.services').hide("slow");
+      $('.howto').hide("slow");
+      $('.goodpractice').hide("slow");
+    });
   });
 
   $('.aboutbutton').click(function(){
-    $('.about').show("slow");
-    $('.contact').hide("slow");
-    $('.home').hide("slow");
-    $('.services').hide("slow");
-    $('.howto').hide("slow");
-    $('.goodpractice').hide("slow");
-	   window.scrollTo(0, 0);
+    $('body').animate({
+        scrollTop: 0
+    }, 600, function(){
+      $('.about').show("slow");
+      $('.contact').hide("slow");
+      $('.home').hide("slow");
+      $('.services').hide("slow");
+      $('.howto').hide("slow");
+      $('.goodpractice').hide("slow");
+    });
   });
 
   $('.contactbutton').click(function(){
-    $('.about').hide("slow");
-    $('.contact').show("slow");
-    $('.home').hide("slow");
-    $('.services').hide("slow");
-    $('.howto').hide("slow");
-    $('.goodpractice').hide("slow");
-	   window.scrollTo(0, 0);
+    $('body').animate({
+        scrollTop: 0
+    }, 600, function(){
+      $('.about').hide("slow");
+      $('.contact').show("slow");
+      $('.home').hide("slow");
+      $('.services').hide("slow");
+      $('.howto').hide("slow");
+      $('.goodpractice').hide("slow");
+    });
   });
 
   $('.servicesbutton').click(function(){
-    $('.about').hide("slow");
-    $('.contact').hide("slow");
-    $('.home').hide("slow");
-    $('.services').show("slow");
-    $('.howto').hide("slow");
-    $('.goodpractice').hide("slow");
-	   window.scrollTo(0, 0);
+    $('body').animate({
+        scrollTop: 0
+    }, 600, function(){
+      $('.about').hide("slow");
+      $('.contact').hide("slow");
+      $('.home').hide("slow");
+      $('.services').show("slow");
+      $('.howto').hide("slow");
+      $('.goodpractice').hide("slow");
+    });
   });
 
   $('.howtobutton').click(function(){
-    $('.about').hide("slow");
-    $('.contact').hide("slow");
-    $('.home').hide("slow");
-    $('.services').hide("slow");
-    $('.howto').show("slow");
-    $('.goodpractice').hide("slow");
-	  window.scrollTo(0, 0);
+    $('body').animate({
+        scrollTop: 0
+    }, 600, function(){
+      $('.about').hide("slow");
+      $('.contact').hide("slow");
+      $('.home').hide("slow");
+      $('.services').hide("slow");
+      $('.howto').show("slow");
+      $('.goodpractice').hide("slow");
+    });
   });
 
   $('.practicebutton').click(function(){
-    $('.about').hide("slow");
-    $('.contact').hide("slow");
-    $('.home').hide("slow");
-    $('.services').hide("slow");
-    $('.howto').hide("slow");
-    $('.goodpractice').show("slow");
-	  window.scrollTo(0, 0);
+    $('body').animate({
+        scrollTop: 0
+    }, 600, function(){
+      // Animation complete.
+      $('.about').hide("slow");
+      $('.contact').hide("slow");
+      $('.home').hide("slow");
+      $('.services').hide("slow");
+      $('.howto').hide("slow");
+      $('.goodpractice').show("slow");
+    });
   });
+
+  /*
+  $(".nav a").on("click", function(){
+   $(".mynavbar").find(".active").removeClass("active");
+   $(this).parent().addClass("active");
+ });*/
 
 }
